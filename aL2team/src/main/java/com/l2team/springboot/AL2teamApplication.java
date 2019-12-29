085039eb2e61bca0a5e7acc16c24dc64ca2bd45c
locdo98 <44898288+locdo98@users.noreply.github.com>
2019-12-29 23:32:11 +0700

    tao project

---
+++ aL2team/src/main/java/com/l2team/springboot/AL2teamApplication.java
@@ -0,0 +1,13 @@
+package com.l2team.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AL2teamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AL2teamApplication.class, args);
+	}
+
+}
---
+++ aL2team/src/test/java/com/l2team/springboot/AL2teamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2team.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AL2teamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
